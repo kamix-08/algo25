caf9c9c1d62d91313c9f3bbf4ff323df569ce911
Kamix-08 <pawlowski.kaka@gmail.com>
2025-09-17 11:01:18 +0200

    express

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3407
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send('a')
+  res.sendFile('index.html', {root: `${__dirname}/public`})
+})
+
+app.get('/sub1', (req, res) => {
+  res.sendFile('sub1.html', {root: `${__dirname}/public/sub`})
+})
+
+app.get('/sub2', (req, res) => {
+  res.sendFile('sub2.html', {root: `${__dirname}/public/sub`})
 })
 
 app.listen(port, () => {
