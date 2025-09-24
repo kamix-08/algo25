0d73e2d2a2ed5c64e439f0549aca6ec1d9441ca3
Kamix-08 <pawlowski.kaka@gmail.com>
2025-09-24 10:57:15 +0200

    moved files around lol

--- js/lekcja01/index.js
+++ js/lekcja01/index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3407
 
-app.use(express.static('public'))
-
 app.get('/', (req, res) => {
   res.sendFile('index.html', {root: `${__dirname}/public`})
 })
@@ -14,6 +12,10 @@ app.get('/sub1', (req, res) => {
 
 app.get('/sub2', (req, res) => {
   res.sendFile('sub2.html', {root: `${__dirname}/public/sub`})
+})
+
+app.get('/css', (req, res) => {
+  res.sendFile('styles.css', {root: `${__dirname}/public`})
 })
 
 app.listen(port, () => {
