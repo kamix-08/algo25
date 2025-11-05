9d3f27d3e9f6d1d21e75d083f93ecb43ffa3d652
Kamix-08 <pawlowski.kaka@gmail.com>
2025-11-05 10:58:42 +0100

    4x web goes crazy

--- js/lekcja04/index.js
+++ js/lekcja04/index.js
@@ -10,12 +10,32 @@ app.set('views', './templates')
 
 app.get('/', (req, res) => {
     res.render('home', {
-        user: 'Kamil',
-        text1: "Hello\nHi\nWelcome",
-        lines: "Welcome\nHi\nHello".split("\n")
+        user: 'Kamil'
     })
 })
 
+app.get('/sub1', (req, res) => {
+    res.render('subpage', {
+        text: 'Subpage #1'
+    })
+})
+
+// tu wchodzimy normalnie na /form
+app.get('/form', (req, res) => {
+    res.render('form')
+})
+
+app.use(express.urlencoded())
+
+// to się wykona po przesłaniu formularza,
+// bo jest method=post
+app.post('/form', (req, res) => {
+    console.log(req.body)
+
+    // to już pójdzie po get
+    res.redirect('/form')
+})
+
 app.listen(port, () => {
-    console.log(port)
+    console.log(`http://127.0.0.1:${port}`)
 })
