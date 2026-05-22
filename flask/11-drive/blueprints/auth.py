cf5ab663f12cee456702c88ab4ff1e730521c8b9
Kamix-08 <pawlowski.kaka@gmail.com>
2026-05-22 12:56:28 +0200

    tk: excel

--- flask/11-drive/blueprints/auth.py
+++ flask/11-drive/blueprints/auth.py
@@ -1,11 +1,29 @@
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, flash, render_template, redirect, url_for, request
 from flask_login import login_required, current_user, login_user
 from extensions import db
+from models import User
 
-auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
+auth_bp = Blueprint('auth', __name__)
 
-@auth_bp.route('/login')
-def login():
+@auth_bp.get('/login')
+def login_get():
     if current_user.is_authenticated:
         return redirect(url_for('drive.dashboard'))
     return render_template('auth_login.html', title='Login')
+
+@auth_bp.post('/login')
+def login_post():
+    if current_user.is_authenticated:
+        return redirect(url_for('drive.dashboard'))
+    
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    user = User.query.filter_by(email=email).first()
+    if not user or not user.check_password(password):
+        flash('Błędny email lub hasło')
+        return redirect(url_for('auth.login_get'))
+    
+    if not user.is_active:
+        flash('To konto jest zablokowane.')
+        return redirect(url_for('auth.login_get'))
