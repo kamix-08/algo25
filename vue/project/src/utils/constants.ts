ed4ac4ba7655d40163e4189e5662e208374d0c70
Kamix-08 <pawlowski.kaka@gmail.com>
2026-06-05 19:47:33 +0200

    coin price change

--- vue/project/src/utils/constants.ts
+++ vue/project/src/utils/constants.ts
@@ -3,3 +3,5 @@ export const RED_TRANSPARENT = 'rgba(255, 99, 132, 0.2)'
 
 export const GREEN = 'rgba(75, 192, 192, 1)'
 export const GREEN_TRANSPARENT = 'rgba(75, 192, 192, 0.2)'
+
+export const DAY = 24 * 60 * 60 * 1000
--- vue/project/src/views/CoinDetailView.vue
+++ vue/project/src/views/CoinDetailView.vue
@@ -1,96 +1,162 @@
 <script setup lang="ts">
-import { watch, ref, onBeforeUnmount } from 'vue'
+import { watch, ref, onBeforeUnmount, onMounted, nextTick } from 'vue'
 import { useRoute } from 'vue-router'
 import { RequestHandler } from '../utils/RequestHandler.ts'
 import Chart from 'chart.js/auto'
-import { RED, RED_TRANSPARENT, GREEN, GREEN_TRANSPARENT } from '../utils/constants.ts'
+import { RED, RED_TRANSPARENT, GREEN, GREEN_TRANSPARENT, DAY } from '../utils/constants.ts'
+import noUiSlider from 'nouislider'
+import 'nouislider/dist/nouislider.css'
 
 const route = useRoute()
 const handler = RequestHandler.getInstance()
 
-const from = ref(new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString().slice(0, 16))
-const to = ref(new Date(Date.now()).toISOString().slice(0, 16))
+const fullData = ref()
 
-const _from = ref(Date.now() - 24 * 60 * 60 * 1000)
-const _to = ref(Date.now())
+function getDateFormat(v: number) {
+    return new Date(v).toISOString().slice(0, 16)
+}
+
+const _from = ref(Date.now() - 365 * DAY)
+const from = ref(getDateFormat(_from.value))
 
 const chartCanvas = ref<HTMLCanvasElement | null>(null)
 let chartInstance: Chart<'line'> | null = null
 
+const daily = ref(true)
+const slider = ref<HTMLElement | null>(null)
+
 function getTimestamp(v: Date) {
-    if ((_to.value - _from.value) > 90 * 24 * 60 * 60 * 1000)
+    if (daily.value)
         return v.toLocaleDateString()
     else
         return v.toLocaleTimeString()
 }
 
-watch(
-    [() => route.params.id, _from, _to],
-    async ([newId]) => {
-        if (!newId) return
+function render(prices: number[][], animation: [boolean, number] = [false, 0]) {
+    if (chartInstance)
+        chartInstance.destroy()
 
-        const values = await handler.get(`/coins/${newId}/market_chart/range`, {
-            vs_currency: 'usd',
-            from: _from.value,
-            to: _to.value
-        })
-
-        if (chartInstance)
-            chartInstance.destroy()
-
-        if (!chartCanvas.value) return
-        
-        chartInstance = new Chart(chartCanvas.value, {
-            type: 'line',
-            data: {
-                labels: values.prices.map((p: number[]) => getTimestamp(new Date(p[0] as number))),
-                datasets: [{
-                    data: values.prices.map((p: number[]) => p[1] as number),
-                    fill: true,
-                    segment: {
-                        borderColor: (ctx) => (ctx.p0.parsed.y as number) >= (ctx.p1.parsed.y as number) ? RED : GREEN,
-                        backgroundColor: (ctx) => (ctx.p0.parsed.y as number) >= (ctx.p1.parsed.y as number) ? RED_TRANSPARENT : GREEN_TRANSPARENT
-                    }
-                }]
-            },
-            options: {
-                responsive: true,
-                scales: {
-                    x: {
+    if (!chartCanvas.value) return
+    
+    chartInstance = new Chart(chartCanvas.value, {
+        type: 'line',
+        data: {
+            labels: prices.map((p: number[]) => daily.value ? new Date(p[0]!).toLocaleDateString() : getTimestamp(new Date(p[0]!))),
+            datasets: [{
+                data: prices.map((p: number[]) => p[1]!),
+                fill: true,
+                segment: {
+                    borderColor: (ctx) => ctx.p0.parsed.y! >= ctx.p1.parsed.y! ? RED : GREEN,
+                    backgroundColor: (ctx) => ctx.p0.parsed.y! >= ctx.p1.parsed.y! ? RED_TRANSPARENT : GREEN_TRANSPARENT
+                }
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                x: {
+                    display: true,
+                    title: {
                         display: true,
-                        title: {
-                            display: true,
-                            text: 'Time'
-                        },
-                        grid: {
-                            display: false
-                        }
+                        text: 'Time'
                     },
-                    y: {
-                        display: true,
-                        title: {
-                            display: true,
-                            text: 'Price (USD)'
-                        }
-                    }
-                },
-                plugins: {
-                    legend: {
+                    grid: {
                         display: false
                     }
                 },
-                elements: {
-                    point: {
-                        radius: 2,
-                        backgroundColor: (ctx) => (ctx.parsed.y as number) >= (ctx.dataset.data[ctx.dataIndex - 1] as number) ? GREEN : RED,
-                        borderColor: (ctx) => (ctx.parsed.y as number) >= (ctx.dataset.data[ctx.dataIndex - 1] as number) ? GREEN : RED
+                y: {
+                    display: true,
+                    title: {
+                        display: true,
+                        text: 'Price (USD)'
                     }
                 }
-            }
-        })
-    },
-    { immediate: true }
-)
+            },
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            elements: {
+                point: {
+                    radius: 2,
+                    backgroundColor: (ctx) => (ctx.parsed.y as number) >= (ctx.dataset.data[ctx.dataIndex - 1] as number) ? GREEN : RED,
+                    borderColor: (ctx) => (ctx.parsed.y as number) >= (ctx.dataset.data[ctx.dataIndex - 1] as number) ? GREEN : RED
+                }
+            },
+            animation: animation[0] ? {
+                delay: (ctx) => ctx.type === 'data' && ctx.mode === 'default' ? ctx.dataIndex * animation[1] / prices.length : 0
+            } : false
+        }
+    })
+}
+
+async function renderHourly() {
+    const data = await handler.get(`/coins/${route.params.id}/market_chart/range`, {
+        vs_currency: 'usd',
+        from: _from.value,
+        to: _from.value + DAY
+    })
+
+    render(data.prices, [true, 250])
+}
+
+function createSlider() {
+    noUiSlider.create(slider.value!, {
+        start: [Date.now() - 365 * DAY, Date.now()],
+        connect: true,
+        range: {
+            min: Date.now() - 365 * DAY,
+            max: Date.now()
+        },
+        tooltips: [
+            { to: (v) => new Date(+v).toLocaleDateString() }, 
+            { to: (v) => new Date(+v).toLocaleDateString() }
+        ],
+        step: 60 * 60 * 1000
+    }).on('update', ([v1, v2]) => {
+        if (+v2! - (+v1!) <= DAY) {
+            from.value = getDateFormat(+v1!)
+            return
+        }
+
+        render(fullData.value.prices.filter((p: number[]) => p[0]! >= +v1! && p[0]! <= +v2!))
+    })
+}
+
+function initDisplay() {
+    createSlider()
+    render(fullData.value.prices, [true, 1000])
+}
+
+watch(daily, async (newVal) => {
+    if (!newVal) {
+        await renderHourly()
+        return
+    }
+
+    await nextTick()
+    initDisplay()
+})
+
+watch(from, (newVal) => {
+    _from.value = new Date(newVal).getTime()
+
+    if (!daily.value)
+        renderHourly()
+    else
+        daily.value = false
+})
+
+onMounted(async () => {
+    fullData.value = await handler.get(`/coins/${route.params.id}/market_chart/range`, {
+        vs_currency: 'usd',
+        from: Date.now() - 365 * DAY,
+        to: Date.now()
+    })
+
+    initDisplay()
+})
 
 onBeforeUnmount(() => {
     if (chartInstance)
@@ -99,19 +165,13 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-    <div>
-        <h1>{{ route.params.id }}</h1>
-        <label>
-            From:
-            <input type="datetime-local" v-model="from" :max="to" />
-        </label>
-        <label>
-            To:
-            <input type="datetime-local" v-model="to" :min="from" :max="Date.now()" />
-        </label>
+    <h1>{{ route.params.id }}</h1>
 
-        <button @click="() => { _from = new Date(from).getTime(); _to = new Date(to).getTime() }">Update Chart</button>
-    </div>
+    <input type="checkbox" v-model="daily" id="daily">
+    <label for="daily">Daily</label>
+
+    <div id="silders" v-if="daily" ref="slider"></div>
+    <input type="datetime-local" v-else v-model="from">
 
     <canvas ref="chartCanvas"></canvas>
 </template>
